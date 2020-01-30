7394fee1f8013225cc0374626b6287bcf4483918
SoniaCWeb <sonia.choudhury@hotmail.co.uk>
2020-01-30 17:28:31 +0000

    security updates in local settings

--- btre/settings.py
+++ btre/settings.py
@@ -146,13 +146,6 @@ MESSAGE_TAGS = {
 }
 
 
-#EMAIL CONFIG
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_HOST_USER=''
-# EMAIL_HOST_PASSWORD=''
-# EMAIL_USE_TLS = True
-
 try:
    from .local_settings import *
 except ImportError:
